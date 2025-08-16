4cf3f403098f448219d741522a92a44f0791b980
aditya-parihar19 <aditypariha2004@gmail.com>
2025-08-16 14:36:12 +0530

    [Backend] Add utils, and some configurations

--- Backend/src/db/connection.js
+++ Backend/src/db/connection.js
@@ -1,9 +1,9 @@
-import mongoose, { mongo } from  "mongoose"
+import mongoose from  "mongoose"
 
 const connectDB = async () => {
     try {
         const connection = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
-        console.log("\nMONGODB CONNECTED! ||DB HOST", connection.connection.host)
+        console.log("\nMONGODB CONNECTED! || DB HOST", connection.connection.host)
     } catch (error) {
         console.error("\nMongodb Connection FAILED !! ", error);
         process.exit(1)
---
+++ README.md
@@ -0,0 +1,12 @@
+# 📚 NoteVerse
+
+**NoteVerse** is your one-stop platform for accessing high-quality **study notes** and **previous year question papers** – all in one place. Whether you're preparing for exams, revising key concepts, or looking for past papers to practice, NoteVerse has got you covered.
+
+---
+
+## 🚀 Features
+
+- ✍️ Access **well-structured notes** for a wide range of subjects
+- 📂 Download **previous year question papers** easily
+
+---
