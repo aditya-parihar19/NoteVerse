9fd860397524d6e2a48206b4baa1bd9784066ac1
aditya-parihar19 <aditypariha2004@gmail.com>
2026-01-04 16:42:54 +0530

    [frontend] Added react-hook-form and minor fixes

--- Frontend/src/api/auth.js
+++ Frontend/src/api/auth.js
@@ -1,7 +1,7 @@
 import { api } from  "./client.js"
 
-const login = async (email, password) => {
-  const res = await api.post("/login", {email, password});
+const login = async ({email, password}) => {
+  const res = await api.post("users/login", {email, password});
   return res.data;
 }
 
